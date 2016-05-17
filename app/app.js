19cfd2f2d48b61337b02e79e3a647d58e8a3437d
Jeevan James <jeevanjj@yahoo.com>
2016-05-17 19:38:54 +0530

    Fixed issues related to relative image URL's

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@
     var app = angular.module('app', ['ui.router']);
 
     app.controller('mainController', function($rootScope) {
+        var main = this;
+
         $rootScope.$on('$stateChangeError', function(ev, toState, toParams, fromState, fromParams, options) {
             console.error(toState, toParams, fromState, fromParams, options);
         });
